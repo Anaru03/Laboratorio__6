b3c014ff4c8d4aae3ccd355020aac5a6da4951ba
Anaru03 <dele22428@uvg.edu.gt>
2023-09-03 06:45:39 -0600

    Función para las cads de imagenes

--- app/src/main/java/com/uvg/laboratorio__6/ScreenGaleria.kt
+++ app/src/main/java/com/uvg/laboratorio__6/ScreenGaleria.kt
@@ -10,8 +10,32 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uvg.laboratorio__6.ui.theme.Laboratorio__6Theme
 
+// Referencias de https://github.com/android/compose-samples/tree/main
+// Referencias de https://developer.android.com/codelabs/basic-android-kotlin-compose-art-space?hl=es-419#3
 class ScreenGaleria : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,20 +45,49 @@ class ScreenGaleria : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Galeria("Android")
+
                 }
             }
         }
     }
 }
 
+data class ArtSpace(val title1: String, val subtitle: String, val imageResId: String, val author: String, val year: Int)
+
+
+
+
+
 @Composable
-fun Galeria(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ImageCard(image: Painter, title: String, author: String, year: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { /* Implement image click action */ },
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.art1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = title, fontWeight = FontWeight.Bold, fontSize = 20.sp)
+            Text(text = "Artista: $author")
+            Text(text = "Año: $year")
+        }
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
