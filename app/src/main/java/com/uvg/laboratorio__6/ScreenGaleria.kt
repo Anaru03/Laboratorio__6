324a28549e0a67984ce9a94f99f4b46c5f1cbb9e
Anaru03 <dele22428@uvg.edu.gt>
2023-09-03 18:11:42 -0600

    Ya funciona el Logout

--- app/src/main/java/com/uvg/laboratorio__6/ScreenGaleria.kt
+++ app/src/main/java/com/uvg/laboratorio__6/ScreenGaleria.kt
@@ -1,5 +1,6 @@
 package com.uvg.laboratorio__6
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,11 +16,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -28,11 +24,11 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.draw.clip
+import com.uvg.laboratorio__6.ui.theme.morado3
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import com.uvg.laboratorio__6.ui.theme.Laboratorio__6Theme
 
@@ -41,6 +37,7 @@ import com.uvg.laboratorio__6.ui.theme.Laboratorio__6Theme
 class ScreenGaleria : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             Laboratorio__6Theme {
                 Surface(
@@ -53,6 +50,7 @@ class ScreenGaleria : ComponentActivity() {
         }
     }
 }
+
 
 data class ArtSpace(val name: String, val author: String, val year: String, @DrawableRes val idImage: Int)
 
@@ -86,35 +84,67 @@ fun ImageCard(image: Painter, title: String, author: String, year: String) {
     }
 }
 
+var isLoggedOut by mutableStateOf(false)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ClickableTextButton(onClick: () -> Unit, content: @Composable () -> Unit) {
+    val context = LocalContext.current
+
+    Card(
+        onClick = {
+            onClick()
+            isLoggedOut = true
+            val intent = Intent(context, Login::class.java)
+            context.startActivity(intent)
+        },
+        modifier = Modifier.clickable { onClick() }
+    ) {
+        content()
+    }
+}
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GalleryScreen() {
     var selectedArtIndex by remember { mutableStateOf(0) }
+
     val galleryImages = listOf(
-        ArtSpace("art1", "Ruth", "2020", R.drawable.art1),
-        ArtSpace("art1", "Image 2", "Artist 2", R.drawable.art2),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art3),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art4),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art5),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art6),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art7),
-        ArtSpace("art1", "Image 3", "Artist 3", R.drawable.art8),
-        ArtSpace("art9", "Image 3", "Artist 3", R.drawable.art9),
-        ArtSpace("art10", "Image 3", "Artist 3", R.drawable.art10),
+        ArtSpace("Diru", "Ruth de León", "2022", R.drawable.art1_1),
+        ArtSpace("Amanecer de las 6:00 am", "Ruth de León", "2023", R.drawable.art2),
+        ArtSpace("Amor eléctrico", "Ruth de León", "2023", R.drawable.art3),
+        ArtSpace("Dos gotas de amor", "Ruth de León", "2023", R.drawable.art4),
+        ArtSpace("Toffee mood", "Ruth de León", "2023", R.drawable.art5),
+        ArtSpace("Atardecer hondureño", "Ruth de León", "2022", R.drawable.art6),
+        ArtSpace("El mejor momento del Cerezo ", "Ruth de León", "2023", R.drawable.art7),
+        ArtSpace("Flores de un bello amor", "Ruth de León", "2023", R.drawable.art8),
+        ArtSpace("Floral Teddy", "Ruth de León", "2023", R.drawable.art9),
+        ArtSpace("Deleite de Toffee ", "Ruth de León", "2023", R.drawable.art10),
     )
+
+
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.tertiary)
     ) {
         TopAppBar(
             title = {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Image(
-                        painter = painterResource(R.drawable.cerrar_sesion2),
-                        contentDescription = null,
-                        modifier = Modifier.size(40.dp)
-                    )
+                    ClickableTextButton(
+                        onClick = {
+                            isLoggedOut = true
+                        }
+                    ) {
+                        Image(
+                            painter = painterResource(R.drawable.cerrar_sesion2),
+                            contentDescription = null,
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(
                         text = "Art Space App",
@@ -126,75 +156,88 @@ fun GalleryScreen() {
                 }
             }
         )
+        Spacer(modifier = Modifier.height(10.dp))
 
-        Spacer(modifier = Modifier.height(16.dp))
+        if (isLoggedOut) {
+            val context = LocalContext.current
+            val intent = Intent(context, Login::class.java)
+            context.startActivity(intent)
+        }
 
 
-        Image(
-            painter = painterResource(id = galleryImages[selectedArtIndex].idImage),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(550.dp)
-                .padding(horizontal = 10.dp)
-                .clip(shape = MaterialTheme.shapes.large)
-        )
+                .fillMaxSize()
+                .weight(1f)
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-
-        val selectedArtwork = galleryImages[selectedArtIndex]
-        Text(
-            text = selectedArtwork.name,
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(start = 180.dp)
-        )
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = "Artista: ${selectedArtwork.author}",
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(start = 160.dp)
-        )
-        Text(
-            text = "Año: ${selectedArtwork.year}",
-            fontSize = 12.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(start = 177.dp)
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
         ) {
-            Button(
-                onClick = {
-                    if (selectedArtIndex > 0) {
-                        selectedArtIndex--
-                    }
-                },
-                enabled = selectedArtIndex > 0
-            ) {
-                Text(text = "Anterior")
-            }
+            Image(
+                painter = painterResource(id = galleryImages[selectedArtIndex].idImage),
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .width(500.dp)
+                    .height(470.dp)
+                    .clip(shape = MaterialTheme.shapes.large)
 
-            Button(
-                onClick = {
-                    if (selectedArtIndex < galleryImages.size - 1) {
-                        selectedArtIndex++
-                    }
-                },
-                enabled = selectedArtIndex < galleryImages.size - 1
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+
+            val selectedArtwork = galleryImages[selectedArtIndex]
+            Text(
+                text = selectedArtwork.name,
+                textAlign = TextAlign.Center,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Artista: ${selectedArtwork.author}",
+                fontSize = 16.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(
+                text = "Año: ${selectedArtwork.year}",
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Text(text = "Siguiente")
+                Button(
+                    onClick = {
+                        if (selectedArtIndex > 0) {
+                            selectedArtIndex--
+                        }
+                    },
+                    enabled = selectedArtIndex > 0
+                ) {
+                    Text(text = "Anterior")
+                }
+
+                Button(
+                    onClick = {
+                        if (selectedArtIndex < galleryImages.size - 1) {
+                            selectedArtIndex++
+                        }
+                    },
+                    enabled = selectedArtIndex < galleryImages.size - 1
+                ) {
+                    Text(text = "Siguiente")
+                }
             }
         }
     }
